9b6016a9cc931c6792ce532beac73cd807a49a55
lynnetteeee <lynnette.oxh@gmail.com>
2024-03-28 17:08:27 +0800

    Remove unused import statements

--- src/main/java/seedu/address/logic/commands/RemoveConfirmation.java
+++ src/main/java/seedu/address/logic/commands/RemoveConfirmation.java
@@ -1,7 +1,5 @@
 package seedu.address.logic.commands;
 
-import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.ui.CommandBox;
 import seedu.address.logic.parser.RemoveCommandParser;
 
