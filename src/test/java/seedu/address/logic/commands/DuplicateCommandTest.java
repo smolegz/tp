e1467b41897041496dd276f471bc45aeff4fee40
Kervyn <73232587+kervyntan@users.noreply.github.com>
2024-04-10 22:59:32 +0800

    Added setup function

--- src/test/java/seedu/address/logic/commands/DuplicateCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DuplicateCommandTest.java
@@ -1,2 +1,20 @@
-package seedu.address.logic.commands;public class DuplicateCommandTest {
+package seedu.address.logic.commands;
+
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.BeforeEach;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+public class DuplicateCommandTest {
+    private Model model;
+    private Model expectedModel;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+    }
+
 }
